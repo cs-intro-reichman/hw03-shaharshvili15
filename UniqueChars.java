cfd366afd5b3e3164f41844b6d417dfa9d60aafd
shaharshvili15 <shahar.shvili@post.runi.ac.il>
2024-01-04 23:09:59 +0200

    write unique chars

--- UniqueChars.java
+++ UniqueChars.java
@@ -11,7 +11,12 @@ public class UniqueChars {
      * unless they are space characters.
      */
     public static String uniqueChars(String s) {
-        // Replace the following statement with your code
-        return null;
+        String newString = "";
+        for(int i = 0 ; i<s.length(); i++){
+           if(newString.indexOf(s.charAt(i)) == -1 || s.charAt(i) == 32){
+               newString += s.charAt(i);
+           }
+        }
+        return newString;
     }
 }
